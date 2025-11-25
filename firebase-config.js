961ef704856040befa4c2c1dff412263831e16b7
nebw2000 <nebw2000@proton.me>
2025-11-25 16:28:19 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
+// Firebase-Konfiguration
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "DEINE_APIKEY",
-  authDomain: "DEIN_AUTHDOMAIN",
-  databaseURL: "DEINE_DATABASEURL",
-  projectId: "DEIN_PROJECTID",
-  storageBucket: "DEIN_STORAGEBUCKET",
-  messagingSenderId: "DEINE_MESSAGINGID",
-  appId: "DEINE_APPID"
+  apiKey: "AIzaSyCXX4CwnX_eRiKBURJeORfGoYI0k0hdHkA",
+  authDomain: "event-plan-c4b36.firebaseapp.com",
+  databaseURL: "https://event-plan-c4b36-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "event-plan-c4b36",
+  storageBucket: "event-plan-c4b36.firebasestorage.app",
+  messagingSenderId: "489710650124",
+  appId: "1:489710650124:web:6a515ffd6ec3d9983aff29",
+  measurementId: "G-CPW8C0JVZX"
 };
 
 const app = initializeApp(firebaseConfig);
