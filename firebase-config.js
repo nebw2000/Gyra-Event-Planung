3e009b3dfce69b31ea610d4e55c2112ff37c5429
nebw2000 <nebw2000@proton.me>
2025-11-25 14:52:51 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Importiere die nötigen Firebase Funktionen
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
+
+// Deine Firebase-Konfiguration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXX4CwnX_eRiKBURJeORfGoYI0k0hdHkA",
+  authDomain: "event-plan-c4b36.firebaseapp.com",
+  databaseURL: "https://event-plan-c4b36-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "event-plan-c4b36",
+  storageBucket: "event-plan-c4b36.appspot.com",
+  messagingSenderId: "489710650124",
+  appId: "1:489710650124:web:05423665acc223a83aff29",
+  measurementId: "G-KRY2MM4JEV"
+};
+
+// Firebase initialisieren
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+export const auth = getAuth(app);
