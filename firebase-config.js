8948e326c5f25357fd8e3b7b658db85a3aaf1507
nebw2000 <nebw2000@proton.me>
2025-11-25 21:18:19 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,61 +1,9 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs, orderBy, query } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
-import { firebaseConfig } from "./firebase-config.js";
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-// Push Nachricht
-function showToast(text) {
-    const box = document.getElementById("toast");
-    box.innerText = text;
-    box.style.display = "block";
-
-    setTimeout(() => {
-        box.style.display = "none";
-    }, 3000);
-}
-
-// ===============================
-// Event speichern
-// ===============================
-async function saveEvent(data) {
-    await addDoc(collection(db, "events"), data);
-    showToast("Event gespeichert");
-    setTimeout(() => {
-        window.location.href = "index.html";
-    }, 800);
-}
-
-// Diese Funktion rufst du in create.html beim Klick auf "Speichern" auf:
-window.saveEvent = saveEvent;
-
-// ===============================
-// Events laden
-// ===============================
-async function loadEvents() {
-    const list = document.getElementById("event-list");
-    if (!list) return;
-
-    const q = query(collection(db, "events"), orderBy("datum", "asc"));
-    const result = await getDocs(q);
-
-    list.innerHTML = "";
-
-    result.forEach(doc => {
-        const e = doc.data();
-        const card = document.createElement("div");
-        card.className = "event-card";
-
-        card.innerHTML = `
-            <b>${e.name}</b><br>
-            Datum: ${e.datum}<br>
-            Leiter: ${e.leiter}<br>
-            Rednerpult: ${e.rednerpult}
-        `;
-
-        list.appendChild(card);
-    });
-}
-
-loadEvents();
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXX4CwnX_eRiKBURJeORfGoYI0k0hdHkA",
+  authDomain: "event-plan-c4b36.firebaseapp.com",
+  databaseURL: "https://event-plan-c4b36-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "event-plan-c4b36",
+  storageBucket: "event-plan-c4b36.appspot.com",
+  messagingSenderId: "489710650124",
+  appId: "1:489710650124:web:6a515ffd6ec3d9983aff29"
+};
