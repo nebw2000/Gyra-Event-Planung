a277002fe9763ed9e59c935582317e6a0626afbc
nebw2000 <nebw2000@proton.me>
2025-11-25 15:13:32 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,7 @@
-// Firebase konfigurieren
+// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-database.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.8.3/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXX4CwnX_eRiKBURJeORfGoYI0k0hdHkA",
@@ -10,12 +10,9 @@ const firebaseConfig = {
   projectId: "event-plan-c4b36",
   storageBucket: "event-plan-c4b36.appspot.com",
   messagingSenderId: "489710650124",
-  appId: "1:489710650124:web:05423665acc223a83aff29",
-  measurementId: "G-KRY2MM4JEV"
+  appId: "1:489710650124:web:05423665acc223a83aff29"
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getDatabase(app);
-
-export { app, auth, db };
+export const db = getDatabase(app);
+export const auth = getAuth(app);
