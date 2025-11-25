223d609d9fabcd0922fb592e5285535d2d04f2ef
nebw2000 <nebw2000@proton.me>
2025-11-25 21:12:54 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,23 +1,11 @@
-// Deine Firebase Config
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-database.js";
-
+// Firebase Konfiguration
 const firebaseConfig = {
-    apiKey: "AIzaSyCXX4CwnX_eRiKBURJeORfGoYI0k0hdHkA",
-    authDomain: "event-plan-c4b36.firebaseapp.com",
-    databaseURL: "https://event-plan-c4b36-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "event-plan-c4b36",
-    storageBucket: "event-plan-c4b36.firebasestorage.app",
-    messagingSenderId: "489710650124",
-    appId: "1:489710650124:web:6a515ffd6ec3d9983aff29",
-    measurementId: "G-CPW8C0JVZX"
+    apiKey: "DEIN_API_KEY",
+    authDomain: "DEIN_AUTH_DOMAIN",
+    projectId: "DEIN_PROJECT_ID",
+    storageBucket: "DEIN_BUCKET",
+    messagingSenderId: "DEINE_SENDER_ID",
+    appId: "DEINE_APP_ID"
 };
 
-// Initialisiere Firebase
-const app = initializeApp(firebaseConfig);
-
-// Realtime Database
-const db = getDatabase(app);
-
-// Export
-export { db };
+export { firebaseConfig };
